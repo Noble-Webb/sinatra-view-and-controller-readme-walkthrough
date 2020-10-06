90622b2acf2a8ac2b58a5fc62461c316005d0ff9
Justin_Webb <Webb.n.justin@gmail.com>
2020-10-06 15:24:41 -0600

    Passed and rendered data toviews from the controller using instance variables.

--- app.rb
+++ app.rb
@@ -1,16 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  #renders the reverse page
   get '/reverse' do
     erb :reverse
   end
 
+  #recieves params hash from the form & renders reversed page
   post '/reverse' do
+    original_string = params["string"]
+    @reversed_string = original_string.reverse
     erb :reversed
   end
 
+  #renders the friends page ##iteration practice
   get '/friends' do
-    # Write your code here!
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
 
+    erb :friends
   end
 end
